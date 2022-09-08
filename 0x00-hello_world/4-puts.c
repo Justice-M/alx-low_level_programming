8daa1ce2ac0678be66522eb67651ef3066508bee
Justice-M <mumejustice@gmail.com>
2022-09-08 14:05:41 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
-#include <stdio.h>
-int main(void)
+#include <stdio.h>                                                                                                                                       
+
+/**                                                                                                                                                      
+
+* To print and return 0 as output                                                                                                                        
+
+*/                                                                                                                                                       
+
+int main(void)                                                                                                                                           
+
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-}
