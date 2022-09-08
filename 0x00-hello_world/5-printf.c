1cd4a84c6ee00a047a1632c5c00ba61db0623e6f
Justice-M <mumejustice@gmail.com>
2022-09-08 15:15:22 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
-include <stdio.h>
+#include <stdio.h>
+
 /**
  * Description: main - Prints out a string to stdout.
  *
@@ -6,6 +7,7 @@ include <stdio.h>
  *
  */
 int main(void)
+
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
