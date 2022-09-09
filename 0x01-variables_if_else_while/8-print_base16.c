ae2f866d16f1df837fcb8e4ebbf2a6441ced913c
Justice-M <mumejustice@gmail.com>
2022-09-09 13:38:05 -0700

    8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,17 +4,23 @@
 /**
  * main - Entry point
  *
- * Description: prints lowercase alphabets in reverse
+ * Description: Print base 16 numbers
  *
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
-int a;
-	for (a = 'z'; a >= 'a'; a--)
+char c;
+char d = '0';
+	while (d <= '9')
 	{
-		putchar(a);
+		putchar(d);
+		d++;
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
 	}
 	putchar('\n');
 	return (0);
